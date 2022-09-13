773ab37451411c77e1232d635c7dcc0001f60d7b
Robervan Souza <robervansouza91@gmail.com>
2022-09-12 23:07:44 -0300

    feito o midia query

--- Perola-Negra-Convites/app/src/App.tsx
+++ Perola-Negra-Convites/app/src/App.tsx
@@ -1,16 +1,21 @@
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "styled-components";
+import theme from "assets/styles/theme";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./router";
 
-import theme from 'assets/styles/theme';
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import './App.css';
-import { BrowserRouter} from "react-router-dom";
-import Router from './router';
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
-     <BrowserRouter>
-     <Router/>.
-     </BrowserRouter>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <Router />
+        </QueryClientProvider>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
